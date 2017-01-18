33e84d0815f42d575a33184e306fe66fe1220097
DungDao <mrmrplaydanbg@gmail.com>
2017-01-18 11:53:36 +0700

    Dua Code len GitHub

---
+++ app/src/main/java/vn/edu/hust/set/dungdao/bai3pushgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.set.dungdao.bai3pushgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
